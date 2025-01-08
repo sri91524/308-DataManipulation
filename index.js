e1948d986640f732f7b4625ac88ceb22c61b048c
sri91524 <sreeravish@gmail.com>
2025-01-08 14:09:48 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,41 @@
+// The initial numbers that must be verified.
+const n1 = 10;
+const n2 = 15;
+const n3 = 20;
+const n4 = 5;
+
+// Check one: add up to 50
+// This is a fairly simple operation using
+// arithmetic operators and a comparison.
+const isSum50 = (n1 + n2 + n3 + n4) == 50;
+
+// Check two: at least two odd numbers
+// Here, we use modulus to check if something is odd.
+// Since % 2 is 0 if even and 1 if odd, we can use
+// arithmetic to count the total number of odd numbers.
+const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+
+// Check three: no number larger than 25
+// This time, we use the OR operator to check
+// if ANY of the numbers is larger than 25.
+const isOver25 = n1 < 25 || n2 < 25 || n3 < 25 || n4 < 25;
+
+// Check four: all unique numbers
+// This is long, and there are more efficient
+// ways of handling it with other data structures
+// that we will review later.
+const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+
+// Here, we put the results into a single variable 
+// for convenience. Note how we negate isOver25 using
+// the ! operator. We could also have tested for 
+// "isUnder25" as an alternative.
+const isValid = isSum50 && isTwoOdd && isOver25 && isUnique;
+
+// Finally, log the results.
+console.log(`Your numbers are ${n1}, ${n2}, ${n3}, ${n4} \n\
+    The sum of 4 numbers is 50 - ${isSum50} \n\
+    Two of the numbers are odd - ${isTwoOdd} \n\
+    No number larger than 25 - ${isOver25} \n\
+    All are unique numbers - ${isUnique} \n\
+    and the numbers are valid according to the provided criteria - ${isValid}`);
